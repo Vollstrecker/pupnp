f0cf9b83080a4a2799338bbec2b9ca8413b5a6c3
Vollstrecker <werner@vollstreckernet.de>
2021-05-02 14:58:33 +0200

    Added test-config preseeding Unified Linux and Mac/Win tests.

---
+++ test-config.cmake
@@ -0,0 +1,36 @@
+set (unspecified_server ON
+	CACHE
+	BOOL
+	"unspecified SERVER header"
+	FORCE
+)
+
+set (blocking_tcp_connections ON
+	CACHE
+	BOOL
+	"blocking TCP connections"
+	FORCE
+)
+
+set (BUILD_TESTING ON
+	CACHE
+	BOOL
+	"Build the tests"
+	FORCE
+)
+
+set (DDOWNLOAD_AND_BUILD_DEPS ON
+	CACHE
+	BOOL
+	"Get all missing stuff"
+	FORCE
+)
+
+if (LINUX)
+	set (open_ssl ON
+		CACHE
+		BOOL
+		"open-ssl support"
+		FORCE
+	)
+endif()
